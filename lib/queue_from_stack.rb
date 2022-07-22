65e6b3d35905aa8861c90db4372d8785d7c7d363
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-07-21 22:16:43 -0700

    commit tree e85ed82b3f2da26e37eb3f0956a4da541755afff

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,34 @@
 require_relative './stack'
 
+class MyQueue
+
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add(item)    
+        while @s2.empty? == false
+            @s1.push(@s2.pop)
+        end
+        @s1.push(item)
+    end
+
+    def remove
+        while @s1.empty? == false
+            @s2.push(@s1.pop)
+        end
+        @s2.pop 
+    end
+
+    def peek
+        while @s1.empty? == false
+            @s2.push(@s1.pop)
+        end
+        @s2.peek 
+    end
+
+
+end
+
 # your code here
